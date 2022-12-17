80cadb6fb2e0efd9c92c9d279dea98ffb72328dc
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-12-17 20:40:46 +0900

    list / detail 페이지 구현

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo 입니다.")
+
+    '''
+    pybo 목록 출력
+    '''
+    
+    question_list = Question.objects.order_by('-create_date') # - 를 붙임으로 써 역순 정렬
+    context = {'question_list': question_list}
+
+    return render(request, 'pybo/question_list.html',context)
+
+def detail(request, question_id):
+
+    '''
+    pybo 내용 출력
+    '''
+
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html',context)

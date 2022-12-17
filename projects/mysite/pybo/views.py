784927930c7295ce961f4cf4475e7ec98ecbe7d4
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-12-17 23:17:13 +0900

    답변등록 기능 추가

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from .models import Answer
+from django.utils import timezone
 
 def index(request):
-
     '''
     pybo 목록 출력
     '''
@@ -14,7 +15,6 @@ def index(request):
     return render(request, 'pybo/question_list.html',context)
 
 def detail(request, question_id):
-
     '''
     pybo 내용 출력
     '''
@@ -22,3 +22,16 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question':question}
     return render(request, 'pybo/question_detail.html',context)
+
+def answer_create(request, question_id):
+    '''
+    pybo 답변 등록
+    '''
+    
+    question = get_object_or_404(Question, pk=question_id)
+    #question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+
+    return redirect('pybo:detail', question_id=question.id)

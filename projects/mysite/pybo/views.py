874f2a9740268418d902e52d2e8f748de4c48dad
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-12-18 22:01:00 +0900

    장고 modelform 생성 및 게시글 등록 기능 추가

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from .models import Answer
 from django.utils import timezone
+from .forms import QuestionForm, AnswerFrom
 
 def index(request):
     '''
@@ -29,9 +30,42 @@ def answer_create(request, question_id):
     '''
     
     question = get_object_or_404(Question, pk=question_id)
-    #question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    ##question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
 
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
+    #answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    #answer.save()
 
-    return redirect('pybo:detail', question_id=question.id)
+    #return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == 'POST':
+        form = AnswerFrom(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerFrom()
+    
+    context = {'question':question, 'form':form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    '''
+    pybo 질문 등록
+    '''
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else :
+        form = QuestionForm()
+
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
+

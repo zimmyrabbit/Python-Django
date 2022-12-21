ac5bb7b88f09fd5d8e6f050b7722c6e2e73b3a31
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-12-21 23:05:57 +0900

    Answer 댓글 등록/수정/삭제 기능 추가

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path('comment/create/question/<int:question_id>', comment_views.comment_create_question, name='comment_create_question'),
     path('comment/modify/question/<int:comment_id>', comment_views.comment_modify_question, name='comment_modify_question'),
     path('comment/delete/question/<int:comment_id>', comment_views.comment_delete_question, name='comment_delete_question'),
+    path('comment/create/answer/<int:answer_id>', comment_views.comment_create_answer, name='comment_create_answer'),
+    path('comment/modify/answer/<int:comment_id>', comment_views.comment_modify_answer, name='comment_modify_answer'),
+    path('comment/delete/answer/<int:comment_id>', comment_views.comment_delete_answer, name='comment_delete_answer'),
 ]

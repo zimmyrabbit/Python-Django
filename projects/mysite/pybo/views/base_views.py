09325b00d6c23e9f6fc0192d56d8bae8a4e4a1b1
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-12-23 17:54:19 +0900

    검색 기능 추가

--- projects/mysite/pybo/views/base_views.py
+++ projects/mysite/pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from ..models import Question
 
 def index(request):
@@ -7,13 +8,23 @@ def index(request):
     pybo 목록 출력
     '''
     page = request.GET.get('page','1')
+    kw = request.GET.get('kw','')
     
     question_list = Question.objects.order_by('-create_date') # - 를 붙임으로 써 역순 정렬
+
+    if kw :
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(answer__content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
 
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page':page, 'kw':kw}
 
     return render(request, 'pybo/question_list.html',context)
 

cb2982828e8f139a6b79eb9ed7334d4325c9b3c3
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-01-02 19:04:33 +0900

    category model추가 및 db migrate

--- projects/mysite/pybo/models.py
+++ projects/mysite/pybo/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    has_answer = models.BooleanField(default=True) #답변 가능 여부
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('pybo:index', args=[self.name])
 
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
@@ -8,6 +20,7 @@ class Question(models.Model):
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_question')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_question')
 
     def __str__(self):
         return self.subject

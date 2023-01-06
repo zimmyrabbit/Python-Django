b56c6d0f481b8c79fb2253bbee173c6bb31b0800
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-01-06 23:17:51 +0900

    마이페이지 작성한 질문 목록 추가

--- projects/mysite/common/urls.py
+++ projects/mysite/common/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('signup/',password_views.signup, name='signup'),
     path('password_reset/', password_views.password_reset_request, name="password_reset"),
     #profile
-    path('profile/base/<int:user_id>/', profile_views.profile_base, name='profile_base'), 
+    path('profile/base/<int:user_id>/', profile_views.profile_base, name='profile_base'),
+    path('profile/question/<int:user_id>/', profile_views.profile_question, name='profile_question'), 
 ]
--- projects/mysite/common/views/profile_views.py
+++ projects/mysite/common/views/profile_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from django.contrib.auth.models import User
+from pybo.models import Question
 
 def profile_base(request, user_id):
     '''
@@ -9,3 +10,12 @@ def profile_base(request, user_id):
     user = get_object_or_404(User,pk=user_id)
     context = {'profile_user':user, 'profile_type':'base'}
     return render(request, 'common/profile/profile_base.html',context)
+
+def profile_question(request, user_id):
+    '''
+    프로필 게시 탭
+    '''
+    user = get_object_or_404(User,pk=user_id)
+    question = Question.objects.filter(author = user_id)
+    context = {'profile_user':user, 'profile_question':question, 'profile_type':'question'}
+    return render(request, 'common/profile/profile_question.html',context)
